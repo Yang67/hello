c399a51214790876a88db0b7be0264275c0d2f6f
yangyang xu <xuyy67@163.com>
2026-04-28 18:47:19 +0800

    feat: add SayHi and SayHello functions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Yang67/hello
+
+go 1.25.9
---
+++ hello.go
@@ -0,0 +1,11 @@
+package hello
+
+import "fmt"
+
+func SayHi() {
+	fmt.Println("你好，很高兴认识你。")
+}
+
+func SayHello() {
+	fmt.Println("hello, nice to meet you!")
+}
